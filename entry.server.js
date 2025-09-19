edd94aac63d2a279564c694da3806fd3880f03d0
Floriaan <gfloriaan616@gmail.com>
2025-09-19 21:40:41 +0200

    Delete entry.server.js

--- entry.server.js
+++
@@ -1,8 +0,0 @@
-import {createApp} from './src/main.js'
-
-export default async function render(url) {
-  const { app, router } = createApp()
-  router.push(url)
-  await router.isReady()
-  return app
-}
